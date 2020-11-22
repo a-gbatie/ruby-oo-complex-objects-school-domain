a59dd98315de97b4dd5f2ef40ed8626bd1abe83b
a-gbatie <ange.batie@gmail.com>
2020-11-21 18:08:06 -0800

    submit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, student_grade)
+        roster[student_grade] ||= []
+        roster[student_grade] << student_name
+        # new_student = @roster.add_student()
+        # @roster << student_name
+        # @roster << student_grade
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        roster.each_value {|student_arr| student_arr.sort!}    
+    end
+
+    
+end
